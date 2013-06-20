eaba4222a41a0461f97f09b0a9d3c4d3ec7bc0ca
kakuzu <kakuzu@live.de>
2013-06-20 17:25:56 +0200

    Update Folder Structur

---
+++ App/www/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * PandaSoft PHP Framework
+ *
+ * Copyright (C) 2013 FoxyNet
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must contain the LICENCE.txt.
+ *
+ * @copyright (c) 2013, FoxyNet
+ * @link      http://board.foxynet.de PandaSoft Support Forum
+ * @version   0.1-dev
+ * @package   App.webroot
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+echo 'It Work´s';
